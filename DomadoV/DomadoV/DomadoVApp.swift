947dce2afe57396134cf6962bb672ec2097ec2fb
fine <92whdtjs@gmail.com>
2024-10-07 21:44:34 +0900

    initial Xcode project

---
+++ DomadoV/DomadoV/DomadoVApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomadoVApp.swift
+//  DomadoV
+//
+//  Created by 이종선 on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DomadoVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DomadoV/DomadoVUITests/DomadoVUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DomadoVUITestsLaunchTests.swift
+//  DomadoVUITests
+//
+//  Created by 이종선 on 10/7/24.
+//
+
+import XCTest
+
+final class DomadoVUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
