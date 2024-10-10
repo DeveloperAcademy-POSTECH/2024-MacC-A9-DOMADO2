5fca034cc5ee9b4292f8c9882ba95cea690e6f16
fine <92whdtjs@gmail.com>
2024-10-10 10:41:33 +0900

    feat/#10 - 화면간 이동 구현

---
+++ DomadoV/DomadoV/View/RideDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RideDetailView.swift
+//  DomadoV
+//
+//  Created by 이종선 on 10/10/24.
+//
+
+import SwiftUI
+
+struct RideDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RideDetailView()
+}
--- DomadoV/DomadoV/View/RideHistoryView.swift
+++ DomadoV/DomadoV/View/RideHistoryView.swift
@@ -7,12 +7,29 @@
 
 import SwiftUI
 
+/// 주행 기록 화면
+///
+/// 1. 현재까지의 주행기록을 리스트의 형태로 보여줍니다.
+/// 2. 각 셀을 눌렀을 때 해당 기록에 대한 상세뷰로 이동합니다.
 struct RideHistoryView: View {
     
     @ObservedObject var vm: RideHistoryViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 20){
+                
+                Button {
+                    vm.returnToPreparation()
+                } label: {
+                    Text("주행 준비 화면으로 돌아가기")
+                }
+
+                NavigationLink(destination: RideDetailView()){
+                    RideHistoryCell()
+                }
+            }
+        }
     }
 }
 
