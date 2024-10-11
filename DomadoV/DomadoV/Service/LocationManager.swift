beccae86d59e69e203490f250320b42900ca076c
fine <92whdtjs@gmail.com>
2024-10-11 11:39:43 +0900

    feat/#19 - LocationManager 구현

--- DomadoV/DomadoV/Service/LocationManager.swift
+++ DomadoV/DomadoV/Service/LocationManager.swift
@@ -5,9 +5,55 @@
 //  Created by 이종선 on 10/8/24.
 //
 
+import CoreLocation
+import Combine
 import Foundation
 
 /// CoreLocation 프레임워크를 활용해서 사용자 속도, 위치 정보등을 가져옵니다.
-class LocationManager {
+class LocationManager: NSObject, CLLocationManagerDelegate {
     
+    /// 싱글톤을 통해 앱내의 하나의 위치 매니저만 사용합니다.
+    static let shared = LocationManager()
+    /// CLLocationManager를 통해 사용자의 위치 정보를 얻습니다.
+    private let locationManager = CLLocationManager()
+    /// LocationManager는 CLLocationMananger로부터 받아온 위치정보를 다시 발행합니다.
+    private let locationSubject = PassthroughSubject<CLLocation, Never>()
+    
+    
+    /// private 생성자를 이용해 외부에서의 초기화를 막아 싱글톤을 유지합니다.
+    /// locationManager의 delegate 지정 및 위치 정보 정확도를 설정합니다.
+    private override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    /// 현재 사용자 위치 정보 권한을 확인합니다.
+    func checkLocationAuthorization() -> CLAuthorizationStatus {
+        return locationManager.authorizationStatus
+    }
+    
+    /// 사용자 위치 정보 권한을 요청합니다.
+    func requestLocationAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    /// 위치정보 수집을 시작합니다.
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    /// 위치저보 수집을 중지합니다.
+    func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    /// 기기의 위치변경이 감지될때마다 새로운 위치를 subject를 통해 발행합니다. 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        locationSubject.send(location)
+    }
+
 }
+
