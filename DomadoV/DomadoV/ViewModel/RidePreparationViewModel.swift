972ce4857424056454328524804246539321f971
fine <92whdtjs@gmail.com>
2024-10-12 00:13:12 +0900

    feat/#25 - vm에 RideSession 프로퍼티 및 생성자 추가

--- DomadoV/DomadoV/ViewModel/RidePreparationViewModel.swift
+++ DomadoV/DomadoV/ViewModel/RidePreparationViewModel.swift
@@ -10,8 +10,13 @@ import Combine
 /// 주행시작화면에 대한 정보 및 동작을 관리합니다.
 class RidePreparationViewModel: ObservableObject, RideEventPublishable {
     
-    /// AppCoordinator에게 RideEvent를 발행하여 화면을 전환합니다. 
+    let rideSession: RideSession
+    /// AppCoordinator에게 RideEvent를 발행하여 화면을 전환합니다.
     let rideEventSubject = PassthroughSubject<RideEvent, Never>()
+    
+    init(rideSession: RideSession) {
+        self.rideSession = rideSession
+    }
     
     /// 주행을 시작합니다.
     func startRide() {
