3efe28ef6c7482a5cbc7a379446fd2da6cd67725
fine <92whdtjs@gmail.com>
2024-10-16 00:02:57 +0900

    feat/#38 - CoreDataManager 구현

--- DomadoV/DomadoV/Persistent/CoreDataManager.swift
+++ DomadoV/DomadoV/Persistent/CoreDataManager.swift
@@ -5,9 +5,58 @@
 //  Created by 이종선 on 10/8/24.
 //
 
+import CoreData
 import Foundation
 
 /// dataModel을 사용해 데이터 구조를 설계하고 CoreData의 PersistentContainer와 Context 기본저장 로직을 관리합니다.
 class CoreDataManager {
     
+    static let shared = CoreDataManager()
+    
+    private init() {}
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "PacepalModel")
+        container.loadPersistentStores { _ , error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    lazy var mainContext: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
+    
+    func saveContext() {
+        saveContext(mainContext)
+    }
+    
+    func saveContext( _ context: NSManagedObjectContext) {
+        if context.parent == mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    
+    func saveDerivedContext(_ context: NSManagedObjectContext) {
+        context.perform { [self] in
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            
+            saveContext(mainContext)
+        }
+    }
 }
