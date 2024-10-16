93d670f60f182da5bf7e2a87429b1fce05692c5e
fine <92whdtjs@gmail.com>
2024-10-16 13:29:52 +0900

    feat/#39 - 엔티티 서브클래스에서 경로 압축 연산 프로퍼티 사용

--- DomadoV/DomadoV/Persistent/Ride+CoreDataClass.swift
+++ DomadoV/DomadoV/Persistent/Ride+CoreDataClass.swift
@@ -8,8 +8,19 @@
 
 import Foundation
 import CoreData
+import CoreLocation
 
 @objc(Ride)
 public class Ride: NSManagedObject {
-
+    
+    // route 데이터를 처리하기 위한 계산 속성
+    public var route: [CLLocationCoordinate2D]? {
+        get {
+            guard let routeData = routeData else { return nil }
+            return LocationCompressionUtility.decompressLocations(routeData)
+        }
+        set {
+            routeData = newValue.flatMap { LocationCompressionUtility.compressLocations($0) }
+        }
+    }
 }
