e349ad1fa95b9e56876d56dadfebc79de37631af
fine <92whdtjs@gmail.com>
2024-10-19 12:27:11 +0900

    feat/#62 - ViewModel에서 시간 포멧터 제거

--- DomadoV/DomadoV/ViewModel/RideSummaryViewModel.swift
+++ DomadoV/DomadoV/ViewModel/RideSummaryViewModel.swift
@@ -54,10 +54,4 @@ class RideSummaryViewModel: ObservableObject, RideEventPublishable {
         ]
     }
     
-    func formatTime(_ seconds: TimeInterval) -> String {
-        let hours = Int(seconds) / 3600
-        let minutes = (Int(seconds) % 3600) / 60
-        let remainingSeconds = Int(seconds) % 60
-        return String(format: "%02d:%02d:%02d", hours, minutes, remainingSeconds)
-    }
 }
