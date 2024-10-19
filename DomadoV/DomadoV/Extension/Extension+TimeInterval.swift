c30c576c65b1edf780a43735836e41af51912537
fine <92whdtjs@gmail.com>
2024-10-19 12:27:57 +0900

    feat/#62 - TimeInterval formatting extension

---
+++ DomadoV/DomadoV/Extension/Extension+TimeInterval.swift
@@ -0,0 +1,24 @@
+//
+//  extension+TimeInterval.swift
+//  DomadoV
+//
+//  Created by 이종선 on 10/19/24.
+//
+
+import Foundation
+
+/// 초단위 시간을 받아 시간 : 분 : 초 형태로 변환합니다. 
+extension TimeInterval {
+    func formatTime() -> String {
+        let totalSeconds = Int(self)
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let remainingSeconds = totalSeconds % 60
+        
+        let hoursString = String(format: "%02d", hours)
+        let minutesString = String(format: "%02d", minutes)
+        let secondsString = String(format: "%02d", remainingSeconds)
+        
+        return "\(hoursString):\(minutesString):\(secondsString)"
+    }
+}
